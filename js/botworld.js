8e1a5e90a7a5dac856e8f58a05867e266695ce58
Connor <connormbrooks@gmail.com>
2016-11-27 16:39:28 -0600

    Added value iteration for determining utility values

--- js/botworld.js
+++ js/botworld.js
@@ -6,17 +6,39 @@ var board;
 
 //board object
 function boardObject(){
+	//matrix keeping track of elements stored by their location
 	this.boardMat = [];
-	this.addRow = function(newRow){
-		this.boardMat.push(newRow);
+
+	//matrix that maps ids to locations
+	this.idLocationMat = [];
+
+	this.addElement = function(el){
+		this.boardMat[el.row+"_"+el.col] = el;
+		this.idLocationMat[""+el.id] = el.row+"_"+el.col;
 	}
 	this.getElement = function(row, col){
-		return this.boardMat[row][col];
+		return this.boardMat[row+"_"+col];
+	}
+	this.getElementById = function(id){
+		return this.boardMat[this.idLocationMat[""+id]];
+	}
+	this.getAccessibleStates = function(){
+		var s = [];
+		for(var i = 0; i < this.rows; i++){
+			for(var j = 0; j < this.cols; j++){
+				if(this.getElement(i,j).accessible){
+					s.push(this.getElement(i,j));
+				}
+			}
+		}
+		return s;
 	}
 	this.startingTile = [0,0];
 	this.endingTile = [1,1];
 	this.rows = 2;
 	this.cols = 2;
+	this.gamma = 0.1;
+	this.threshold = 1;
 }
 
 function boardSpace(id, row, col){
@@ -32,6 +54,9 @@ function boardSpace(id, row, col){
 //OBJECTS*********************************************************************
 
 
+
+//MAIN FUNCTIONS*********************************************************************
+
 function boardClick(div){
 	var divId = div.id;
 	var row = parseInt(divId.split(":")[0]);
@@ -39,6 +64,67 @@ function boardClick(div){
 	var el = board.getElement(row,col);
 	displayOptions(el);
 }
+
+function policy(){
+	var gamma = parseFloat(document.getElementsByName("gamma")[0].value);
+	if(isNaN(gamma) || gamma < 0 || gamma > 1){
+		alert("Gamma must be a number between 0 and 1");
+	} else {
+		var threshold = parseFloat(document.getElementsByName("threshold")[0].value);
+		if(isNaN(threshold) || threshold < 0){
+			alert("Threshold must be a positive number");
+		} else {
+			board.gamma = gamma;
+			board.threshold = threshold;
+			valueIteration();
+			addRunButton();
+		}
+	}
+}
+
+function valueIteration(){
+	var s = board.getAccessibleStates();
+	var util = startingUtilities(s);
+	iterate(s, util, 1);
+}
+
+function iterate(s, util_prime, count){
+	var util = deepCopy(util_prime);
+	var delta = 0;
+	for(var i = 0; i < s.length; i++){
+		util_prime[""+s[i].id] = board.getElementById(s[i].id).score 
+		+ board.gamma*Math.max(scoreAction(s[i],"UP",util),scoreAction(s[i],"RIGHT",util), scoreAction(s[i],"DOWN",util),scoreAction(s[i],"LEFT",util));
+		if(Math.abs(util_prime[""+s[i].id] - util[""+s[i].id]) > delta){
+			delta = Math.abs(util_prime[""+s[i].id] - util[""+s[i].id]);
+		}
+	}
+	if(delta >= (board.threshold*(1-board.gamma))/board.gamma){
+		//iterate again
+		setTimeout(function(){
+			iterate(s, util_prime, ++count);
+		},100);
+	} else {
+		//done iterating, set utilities and end
+		setUtilities(util_prime);
+	}
+}
+
+function run(){
+	console.log("run");
+}
+
+function scoreAction(state, action){
+	return 0;
+}
+
+//MAIN FUNCTIONS*********************************************************************
+
+
+
+//HTML MODIFIERS*********************************************************************
+
+//This function creates a matrix of appropriate size to use for internal model of game
+//also modifies HTML to create divs for gameboard on screen
 
 function validateInput(){
 	var rows = parseInt(document.getElementsByName('size')[0].value);
@@ -72,6 +158,8 @@ function updateProbability(row, col){
 	var probability = parseFloat(document.getElementsByName('probability')[0].value);
 	if(isNaN(probability)){
 		alert("Invalid number format for score");
+	} else if(probability < 0 || probability > 1){
+		alert("Probability of successful move must be between 0 and 1");
 	} else{
 		board.getElement(row,col).probability = probability;
 		document.getElementById(row+":"+col+"probability").innerHTML = probability;
@@ -79,43 +167,6 @@ function updateProbability(row, col){
 	} 
 }
 
-function policy(){
-	var gamma = parseFloat(document.getElementsByName("gamma")[0].value);
-	if(isNaN(gamma) || gamma < 0 || gamma > 1){
-		alert("Gamma must be a number between 0 and 1");
-	} else {
-		var threshold = parseFloat(document.getElementsByName("threshold")[0].value);
-		if(isNaN(threshold) || threshold < 0){
-			alert("Threshold must be a positive number");
-		} else {
-			board.gamma = gamma;
-			board.threshold = threshold;
-			valueIteration();
-			addRunButton();
-		}
-	}
-}
-
-function valueIteration(){
-	util = startingUtilities();
-	util2 = deepCopy(util);
-	delta = 0;
-	do{
-		for(var i = 0; i < util.length; i++){
-			//TODO: finish value iteration
-		}
-	} while(delta >= (board.threshold*(1-board.gamma))/board.gamma);
-	setUtilities(util2);
-}
-
-function run(){
-	console.log("run");
-}
-
-//HTML MODIFIERS*********************************************************************
-
-//This function creates a matrix of appropriate size to use for internal model of game
-//also modifies HTML to create divs for gameboard on screen
 function generateBoard(rows, cols){
 	var boardElement = document.getElementById("Board");
 	var size = 90/(cols);
@@ -128,18 +179,17 @@ function generateBoard(rows, cols){
 	board.cols = cols;
 	var counter = 1;
 	for(var i = 0; i < rows; i++){
-		var row = [];
 		boardElement.innerHTML = boardElement.innerHTML + "<div id='Row"+i
 			+"' style='padding-left: "+padding+"vw; float: left;'></div>";
 		var rowElement = document.getElementById("Row"+i);
 		for(var j = 0; j < cols; j++){
-			row.push(new boardSpace(counter++, i, j));
+			board.addElement(new boardSpace(counter++, i, j));
 			rowElement.innerHTML = rowElement.innerHTML + "<div id='"+i+":"+j
 				+"' class='boardspace' onclick='boardClick(this)' style='width: "
 				+size+"vw; min-height: "+size+"vw; float: left'><span id='"+i+":"+j+"status"+"'>"
-				+"</span><br><span id='"+i+":"+j+"score'>0</span>/<span id='"+i+":"+j+"probability'>1</span></div>";
+				+"</span><br><span id='"+i+":"+j+"score'>0</span>/<span id='"+i+":"+j+"probability'>1</span>"
+				+"<br/><span id='"+i+":"+j+"util'>0</div>";
 		}
-		board.addRow(row);
 	}
 	document.getElementById("0:0status").innerHTML = "START";
 	document.getElementById("1:1status").innerHTML = "END";
@@ -225,6 +275,10 @@ function makeInaccessible(row, col){
 	}
 }
 
+function drawUtility(row, col, util){
+	document.getElementById(row+":"+col+"util").innerHTML = util;
+}
+
 function displayOptions(el){
 	var infoSpace = document.getElementById("BottomInfoPanel");
 	var str="Tile ("+el.row+","+el.col+")<br/><button onclick='displayUpdateScore("+el.row+","+el.col+")'>Update Score</button>"
@@ -250,33 +304,65 @@ function addRunButton(){
 
 //MDP HELPER FUNCTIONS*********************************************************************
 
+function scoreAction(state, action, util){
+	//sum up four possible results from an action (intended action + 3 unintended actions)
+	var actionMove;
+	if(action == "UP"){
+		actionMove = [[-1, 0],[0, 1],[1, 0], [0, -1]];
+	} else if (action == "RIGHT"){
+		actionMove = [[0, 1], [-1, 0], [1, 0], [0, -1]];
+	} else if (action == "DOWN"){
+		actionMove = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+	} else if (action == "LEFT"){
+		actionMove = [[0, -1], [-1, 0], [0, 1], [1, 0]];
+	} else {
+		console.log("Invalid action");
+		return 0;
+	}
+	var probSuccess = state.probability;
+	var probOther = (1 - probSuccess) / 3.0;
+	var scoreSum = probSuccess*util[""+resultingState(state, actionMove[0]).id];
+	for(var i = 1; i < actionMove.length; i++){
+		scoreSum += probOther*util[""+resultingState(state, actionMove[i]).id];
+	}
+	return scoreSum;
+}
+
+function resultingState(state, action){
+	var newRow = state.row + action[0];
+	var newCol = state.col + action[1];
+	if(newRow < 0 || newRow >= board.rows || newCol < 0 || newCol >= board.cols || !board.getElement(newRow,newCol).accessible){
+		//out of bounds or inaccessible
+		return state;
+	} else {
+		return board.getElement(newRow, newCol);
+	}
+}
+
 function setUtilities(vec){
-	var place = 0;
-	for(var i = 0; i < board.rows; i++){
-		for(var j = 0; j < board.cols; j++){
-			var ele = board.getElement(i,j);
-			if(ele.accessible){
-				ele.utility = vec[place++];
-			}
-		}
+	var ids = Object.keys(vec);
+	for(var i = 0; i < ids.length; i++){
+		var boardEl = board.getElementById(ids[i]);
+		boardEl.utility = vec[ids[i]];
+		drawUtility(boardEl.row, boardEl.col, boardEl.utility);
 	}
 }
 
-function startingUtilities(){
+function startingUtilities(states){
 	util = [];
-	for(var i = 0; i < board.rows; i++){
-		for(var j = 0; j < board.cols; j++){
-			if(board.getElement(i,j).accessible){
-				util.push(0);
-			}
-		}
+	for(var i = 0; i < states.length; i++){
+		util[""+states[i].id] = 0;
 	}
+	return util;
 }
 
-function deepCopy(vec, newvec){
-	for(var i = 0; i < vec.length; i++){
-		newvec.push(vec[i]);
+function deepCopy(obj){
+	var newobj = [];
+	var properties = Object.keys(obj);
+	for(var i = 0; i < properties.length; i++){
+		newobj[properties[i]] = obj[properties[i]];
 	}
+	return newobj;
 }
 
 //MDP HELPER FUNCTIONS*********************************************************************
