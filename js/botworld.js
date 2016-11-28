c426dec54ff76125992554efdbc8f923495bb713
Connor <connormbrooks@gmail.com>
2016-11-28 00:57:06 -0600

    Added policy finding using utility and simulation running capabilities

--- js/botworld.js
+++ js/botworld.js
@@ -50,6 +50,7 @@ function boardSpace(id, row, col){
 	this.probability = 1;
 	this.utility = 0;
 	this.endingTile = false;
+	this.policy = "-";
 }
 
 //OBJECTS*********************************************************************
@@ -78,7 +79,6 @@ function policy(){
 			board.gamma = gamma;
 			board.threshold = threshold;
 			valueIteration();
-			addRunButton();
 		}
 	}
 }
@@ -86,6 +86,7 @@ function policy(){
 function valueIteration(){
 	var s = board.getAccessibleStates();
 	var util = startingUtilities(s);
+	setIteratingStatus();
 	iterate(s, util, 1);
 }
 
@@ -103,19 +104,81 @@ function iterate(s, util_prime, count){
 		//iterate again
 		setTimeout(function(){
 			iterate(s, util_prime, ++count);
-		},100);
+		},10);
 	} else {
-		//done iterating, set utilities and end
+		//done iterating, show calculated utilities of each spot
 		setUtilities(util_prime);
+		//then specify policy
+		setPolicy(util_prime);
+		addRunButton(count);
+	}
+}
+
+function setPolicy(util){
+	var keys = Object.keys(util);
+	for(var i = 0; i < keys.length; i++){
+		var state = board.getElementById(keys[i]);
+		var moves = ["UP", "RIGHT", "DOWN", "LEFT"];
+		//mix up moves to make it random which one is chosen as best when there are equal utilities
+		moves = shuffleArr(moves, 10);
+		var bestMove = moves[0][0];
+		var bestScore = qScoreAction(state, moves[0], util);
+		for(var j = 1; j < moves.length; j++){
+			var newScore = qScoreAction(state, moves[j], util);
+			if(newScore > bestScore){
+				bestMove = moves[j][0];
+				bestScore = newScore;
+			} else if (newScore == bestScore){
+				bestMove += "/"+moves[j][0];
+			}
+		}
+		state.policy = bestMove;
+		drawPolicy(state, bestMove);
 	}
 }
 
 function run(){
-	console.log("run");
+	setCurrentState(board.startingTile,null);
+	setTimeout(function(){
+		getNextState(board.startingTile, 0);
+	}, 100);
 }
 
-function scoreAction(state, action){
-	return 0;
+function getNextState(tile, score){
+	var el = board.getElement(tile[0], tile[1]);
+	score += el.score;
+	if(el.endingTile){
+		endRun(tile, score);
+		return;
+	}
+	var optimum = getPolicyMove(tile);
+	var altMoves = getAltMoves(optimum);
+	var probSuccess = board.getElement(tile[0], tile[1]).probability;
+	var probEachOther = (1-probSuccess)/2.0;
+
+	var randomRoll = Math.random();
+	if(randomRoll < probSuccess){
+		//take the best move
+		var newTile = [tile[0] + optimum[0], tile[1] + optimum[1]];
+	} else if (randomRoll < probSuccess + probEachOther){
+		//take the first alternate move
+		var newTile = [tile[0] + altMoves[0][0], tile[1] + altMoves[0][1]];
+	} else {
+		//take the second alternate move
+		var newTile = [tile[0] + altMoves[1][0], tile[1] + altMoves[1][1]];
+	}
+
+	//check if the chosen move results in an inaccessible/out of bounds state. if so, stay still
+	if(newTile[0] < 0 || newTile[0] >= board.rows || newTile[1] < 0 || newTile[1] >= board.cols 
+		|| !board.getElement(newTile[0],newTile[1]).accessible){
+		newTile = tile;
+	}
+
+	//make move
+	setCurrentState(newTile,tile);
+	setTimeout(function(){
+		getNextState(newTile, score);
+	}, 100);
 }
 
 //MAIN FUNCTIONS*********************************************************************
@@ -126,6 +189,20 @@ function scoreAction(state, action){
 
 //This function creates a matrix of appropriate size to use for internal model of game
 //also modifies HTML to create divs for gameboard on screen
+
+function endRun(tile, score){
+	document.getElementById("TopInfoPanel").innerHTML="Final score: "+score+"<br/><button onclick='run()'>Run Again</button>";
+	setTimeout(function(){
+		document.getElementById(tile[0]+":"+tile[1]).style["background-color"] = "white";
+	}, 1000);
+}
+
+function setCurrentState(tile,lastTile){
+	if(lastTile != null){
+		document.getElementById(lastTile[0]+":"+lastTile[1]).style["background-color"] = "white";
+	}
+	document.getElementById(tile[0]+":"+tile[1]).style["background-color"] = "blue";
+}
 
 function validateInput(){
 	var rows = parseInt(document.getElementsByName('size')[0].value);
@@ -150,7 +227,7 @@ function updateScore(row, col){
 		alert("Invalid number format for score");
 	} else{
 		board.getElement(row,col).score = score;
-		document.getElementById(row+":"+col+"score").innerHTML = score;
+		document.getElementById(row+":"+col+"score").innerHTML = Math.round(score*1000)/1000.0;
 		document.getElementById("BottomInfoPanel").innerHTML = "";
 	} 
 }
@@ -163,7 +240,7 @@ function updateProbability(row, col){
 		alert("Probability of successful move must be between 0 and 1");
 	} else{
 		board.getElement(row,col).probability = probability;
-		document.getElementById(row+":"+col+"probability").innerHTML = probability;
+		document.getElementById(row+":"+col+"probability").innerHTML = Math.round(probability*1000)/1000.0;
 		document.getElementById("BottomInfoPanel").innerHTML = "";
 	} 
 }
@@ -189,7 +266,7 @@ function generateBoard(rows, cols){
 				+"' class='boardspace' onclick='boardClick(this)' style='width: "
 				+size+"vw; min-height: "+size+"vw; float: left'><span id='"+i+":"+j+"status"+"'>"
 				+"</span><br><span id='"+i+":"+j+"score'>0</span>/<span id='"+i+":"+j+"probability'>1</span>"
-				+"<br/><span id='"+i+":"+j+"util'>0</div>";
+				+"<br/><span id='"+i+":"+j+"util'>0</span><br/><span id='"+i+":"+j+"policy'>-</span></div>";
 		}
 	}
 	document.getElementById("0:0status").innerHTML = "START";
@@ -257,7 +334,6 @@ function toggleEndingTile(row, col){
 		document.getElementById("BottomInfoPanel").innerHTML = "";
 	}
 	addPolicyButton();
-	console.log(board.endingTiles);
 }
 
 function makeAccessible(row, col){
@@ -272,7 +348,7 @@ function makeAccessible(row, col){
 
 function makeInaccessible(row, col){
 	if((board.startingTile[0] == row && board.startingTile[1] == col)
-		|| (board.endingTile[0] == row && board.endingTile[1] == col)){
+		|| (board.getElement(row,col).endingTile)){
 		alert("Cannot make starting/ending tile inaccessible");
 	} else {
 		var elem = board.getElement(row, col);
@@ -285,8 +361,12 @@ function makeInaccessible(row, col){
 	}
 }
 
+function drawPolicy(state, policy){
+	document.getElementById(state.row+":"+state.col+"policy").innerHTML = policy;
+}
+
 function drawUtility(row, col, util){
-	document.getElementById(row+":"+col+"util").innerHTML = util;
+	document.getElementById(row+":"+col+"util").innerHTML = Math.round(util*1000)/1000.0;
 }
 
 function displayOptions(el){
@@ -306,15 +386,53 @@ function addPolicyButton(){
 	document.getElementById("TopInfoPanel").innerHTML = str;
 }
 
-function addRunButton(){
-	document.getElementById("TopInfoPanel").innerHTML = "<button onclick='run()'>Run</button>";
+function setIteratingStatus(){
+	document.getElementById("TopInfoPanel").innerHTML = "Performing value iteration...";
+}
+
+function addRunButton(c){
+	document.getElementById("TopInfoPanel").innerHTML = "Utility convergence after: "+c+" iterations<br/><button onclick='run()'>Run</button>";
 }
 
 //HTML MODIFIERS*********************************************************************
 
 //MDP HELPER FUNCTIONS*********************************************************************
 
+function getPolicyMove(tile){
+	var policy = board.getElement(tile[0],tile[1]).policy;
+	var policy_split = policy.split("/");
+	//choose a random move in the case of multiple moves in policy
+	var move = policy_split[Math.floor(Math.random()*policy_split.length)];
+
+	if(move == "U"){
+		return [-1,0];
+	} else if(move == "R"){
+		return [0,1];
+	} else if(move == "D"){
+		return [1,0];
+	} else if(move == "L"){
+		return [0,-1];
+	} else {
+		console.log("Error in policy");
+		return [0,0];
+	}
+}
+
+//finds moves to relative left and right of an intended move
+function getAltMoves(move){
+	if(move[0] != 0){
+		return [[0,1],[0,-1]];
+	} else {
+		return [[1,0],[-1,0]];
+	}
+}
+
 function scoreAction(state, action, util){
+	//first check if this is an ending tile
+	if(state.endingTile){
+		return 0;
+	}
+
 	//sum up four possible results from an action (intended action + 3 unintended actions)
 	var actionMove;
 	if(action == "UP"){
@@ -334,6 +452,37 @@ function scoreAction(state, action, util){
 	var scoreSum = probSuccess*util[""+resultingState(state, actionMove[0]).id];
 	for(var i = 1; i < actionMove.length; i++){
 		scoreSum += probOther*util[""+resultingState(state, actionMove[i]).id];
+	}
+	return scoreSum;
+}
+
+function qScoreAction(state, action, util){
+	//first check if this is an ending tile
+	if(state.endingTile){
+		return 0;
+	}
+
+	//sum up four possible results from an action (intended action + 3 unintended actions)
+	var actionMove;
+	if(action == "UP"){
+		actionMove = [[-1, 0], [0, 1], [0, -1]];
+	} else if (action == "RIGHT"){
+		actionMove = [[0, 1], [-1, 0], [1, 0]];
+	} else if (action == "DOWN"){
+		actionMove = [[1, 0], [0, 1], [0, -1]];
+	} else if (action == "LEFT"){
+		actionMove = [[0, -1], [-1, 0], [1, 0]];
+	} else {
+		console.log("Invalid action");
+		return 0;
+	}
+	var probSuccess = state.probability;
+	var probOther = (1 - probSuccess) / 2.0;
+	var newState = resultingState(state, actionMove[0]);
+	var scoreSum = probSuccess*(newState.score+board.gamma*util[""+newState.id]);
+	for(var i = 1; i < actionMove.length; i++){
+		newState = resultingState(state, actionMove[i]);
+		scoreSum += probOther*(newState.score+board.gamma*util[""+newState.id]);
 	}
 	return scoreSum;
 }
@@ -373,6 +522,17 @@ function deepCopy(obj){
 		newobj[properties[i]] = obj[properties[i]];
 	}
 	return newobj;
+}
+
+function shuffleArr(array, slices){
+	for(var i = 0; i < slices; i++){
+		var mover = Math.floor(Math.random() * array.length);
+		var second_mover = Math.floor(Math.random() * array.length);
+		var temp = array[mover];
+		array[mover] = array[second_mover];
+		array[second_mover] = temp;
+	}
+	return array;
 }
 
 //MDP HELPER FUNCTIONS*********************************************************************
